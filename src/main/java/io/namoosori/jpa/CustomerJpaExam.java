ddc4f05f4375e40cfff8469f7c2baf0061b138cc
kyh012412 <kyh0124.12@gmail.com>
2024-02-19 16:18:33 +0900

    jap 1-5 end

--- src/main/java/io/namoosori/jpa/CustomerJpaExam.java
+++ src/main/java/io/namoosori/jpa/CustomerJpaExam.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import org.hibernate.internal.build.AllowSysOut;
 
 public class CustomerJpaExam {
 
@@ -14,12 +15,29 @@ public class CustomerJpaExam {
         EntityTransaction tx = em.getTransaction();
 
         tx.begin();
-        //실제넣을객체를 persist로 전달
-        em.persist(Customer.sample());
-        tx.commit(); // 문제가 있었을때는 tx.rollback()
-        System.out.println("커밋완료 추정");
-        em.close();
+        try{
+            //실제넣을객체를 persist로 전달
+            //em.persist(Customer.sample());
+
+            Customer foundCustomer = em.find(Customer.class, "ID0001");
+            //System.out.println(foundCustomer.toString());
+
+//            foundCustomer.setName("Park");
+            em.remove(foundCustomer);
+
+//            em.persist(foundCustomer);
+
+            tx.commit();
+            System.out.println("커밋완료");
+        }catch(Exception e){
+            // 문제가 있었을때는 tx.rollback()
+            tx.rollback();
+            System.out.println("커밋실패 & 롤백 가동");
+        }finally {
+            em.close();
+        }
         emf.close();
+
 
     }
 }
--- src/main/java/io/namoosori/jpa/entity/Customer.java
+++ src/main/java/io/namoosori/jpa/entity/Customer.java
@@ -3,7 +3,12 @@ package io.namoosori.jpa.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.*;
 
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
 @Entity//Customer는 테이블에 들어갈 entity임을 명시
 @Table(name="customer_tb")//(어느테이블에 들어갈지) 들어가게 될 테이블명 명시
 public class Customer {
@@ -13,6 +18,9 @@ public class Customer {
     private String name;
     private long registerDate;
 
+    //디폴트 생성자가 없는 상태
+    //그래서 entitymanager가 객체를 생성할 수 없는 상태
+    @Builder // 이게 있어야 select시 담아올 자료형으로 쓸수있음
     public Customer(String id,String name){
         this.id=id;
         this.name =name;
