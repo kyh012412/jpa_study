3673539f0badeb094dce53cde5770437924ac89b
학원에서kyh012412 <kyh0124.12@gmail.com>
2024-02-20 12:38:00 +0900

    jpa 3-4 end

--- src/main/java/io/namoosori/jpa/CustomerJpaExam.java
+++ src/main/java/io/namoosori/jpa/CustomerJpaExam.java
@@ -14,21 +14,21 @@ public class CustomerJpaExam {
         EntityTransaction tx = em.getTransaction();
 
         tx.begin();
-        try{
-            for(int i=0;i<100;i++){
-                Customer customer = new Customer(); //비영속 상태(new)
-                customer.setName("Kim");
-                customer.setRegisterDate(System.currentTimeMillis());
 
-                //실질 id를 알기위해서 이시점에 insert가 됨
-                em.persist(customer); //Customer 객체가 영속 상태(Managed) 가 된다.
-            }
+        try{
+            Customer customer = new Customer(); //비영속 상태(new)
+            customer.setName("Kim");
+            customer.setRegisterDate(System.currentTimeMillis());
+
+            //실질 id를 알기위해서 이시점에 insert가 됨
+            em.persist(customer); //Customer 객체가 영속 상태(Managed) 가 된다.
 
             System.out.println("=========Before Commit========");
 
             //flush와 commit의 개념 구분이 필요
             tx.commit(); // 이때 인설트됨 // h2-conosle에서 db를 바라봐도 올바른 조회가 가능
             System.out.println("커밋완료");
+
         }catch(Exception e){
             // 문제가 있었을때는 tx.rollback()
             tx.rollback();
--- src/main/java/io/namoosori/jpa/entity/Customer.java
+++ src/main/java/io/namoosori/jpa/entity/Customer.java
@@ -9,18 +9,13 @@ import lombok.*;
 @Setter
 @Entity//Customer는 테이블에 들어갈 entity임을 명시
 @Table(name="customer_tb")//(어느테이블에 들어갈지) 들어가게 될 테이블명 명시
-@SequenceGenerator(
-        name="customer_generator",
-        sequenceName = "customer_sq",
-        initialValue = 1,
-        allocationSize = 50)
 public class Customer {
 
     @Id //pk임을 알려줌
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "my_seq")
-    @SequenceGenerator(name="my_seq",sequenceName = "db_seq")
+    @Column(name = "user_id",nullable = false,length = 1)
     private Long id;
     private String name;
+    //@Column 어노테이션 없으면 해당필드는 기본적으로 @Basic어노테이션임
     private long registerDate;
 
     //디폴트 생성자가 없는 상태
